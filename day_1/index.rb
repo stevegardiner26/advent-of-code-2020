f6a957a9c691ea6742e9ccca55b8cc9a4165fe65
Steve Gardiner <stevegardiner26@aol.com>
2020-12-03 16:24:54 -0500

    Added day_1 part_1

---
+++ day_1/index.rb
@@ -0,0 +1,20 @@
+# Reading the input file, find two numbers that add up to 2020
+
+file = File.open("input.txt")
+input_data = file.readlines.map(&:chomp)
+hash = []
+found1 = 0
+found2 = 0
+
+input_data.each do |num|
+  hash[num.to_i] = true
+end
+
+input_data.each do |num|
+  if hash[2020 - num.to_i]
+    found1 = num
+    found2 = 2020 - num.to_i
+  end
+end
+
+puts "Numbers that add up to 2020: #{found1} & #{found2}"
